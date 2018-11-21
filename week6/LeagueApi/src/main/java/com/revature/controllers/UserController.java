684b9cc1c9eb20e959564635c3332cdf373a2688
Blake Kruppa <btkruppa513@gmail.com>
2018-11-21 13:32:38 -0600

    changed something in the ctrl

--- week6/LeagueApi/src/main/java/com/revature/controllers/UserController.java
+++ week6/LeagueApi/src/main/java/com/revature/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,5 +36,6 @@ public class UserController {
 	@PutMapping
 	public AppUser update(@Valid @RequestBody AppUser user) {
 		return us.save(user);
+		
 	}
 }
